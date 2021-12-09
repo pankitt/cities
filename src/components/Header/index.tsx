363ec3a21575b38da23f026267f05706276b3f52
i.ustinchenko <delfa07@bigmir.net>
2021-12-09 15:42:49 +0200

    05

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './index.module.css';
+
+const Header = (): JSX.Element => <div className={styles.wrapper}>Header</div>;
+
+export default Header;
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './index.module.css';
+
+const Main = (): JSX.Element => <div className={styles.wrapper}>Main</div>;
+
+export default Main;
