d4ad1bbad4f26575916472a098329ffeb060f737
i.ustinchenko <delfa07@bigmir.net>
2021-12-24 12:04:35 +0200

    38.1

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -3,14 +3,11 @@ import { ILink } from 'types';
 export const geoSearchParams = (link: Array<ILink> = []) => {
   const findCurrentQuantity = link.find(({ rel }) => rel === 'next');
   const findLastQuantity = link.find(({ rel }) => rel === 'last');
-  const isRegions = findLastQuantity?.href.search('regions') !== -1;
 
   const currentLink = new URLSearchParams(findCurrentQuantity?.href.replace('?', '?&'));
   const lastLink = new URLSearchParams(findLastQuantity?.href.replace('?', '?&'));
   const limit = Number(lastLink.get('limit'));
-  const offsetLast = isRegions
-    ? Number(lastLink.get('offset')) + limit * 2
-    : Number(lastLink.get('offset')) + limit;
+  const offsetLast = Number(lastLink.get('offset')) + limit * 2;
   const offsetCurrent = findCurrentQuantity ? Number(currentLink.get('offset')) : offsetLast;
 
   return { limit, offsetCurrent, offsetLast };
