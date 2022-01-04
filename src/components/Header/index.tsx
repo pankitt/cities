97ff417e5ab3c4bfdfbf9972ad98699479a16fa0
i.ustinchenko <delfa07@bigmir.net>
2022-01-04 18:02:05 +0200

    47.2

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,33 +1,38 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { MdOutlineHomeWork } from 'react-icons/md';
+import { useI18n } from 'hooks';
 import Language from './Language';
 import { Page } from 'common/constants';
 import styles from './index.module.css';
 
-const Header = (): JSX.Element => (
-  <div className={styles.wrapper}>
-    <div className={styles.inner}>
-      <nav>
-        <Link to={Page.HOME} className={styles.link}>
-          <MdOutlineHomeWork className={styles.icon} />
-        </Link>
-        <NavLink
-          to={Page.COUNTRIES}
-          className={({ isActive }) => (isActive ? styles.linkActive : styles.link)}
-        >
-          Countries
-        </NavLink>
-        <NavLink
-          to={Page.CITIES}
-          className={({ isActive }) => (isActive ? styles.linkActive : styles.link)}
-        >
-          Cities
-        </NavLink>
-      </nav>
-      <Language />
+const Header = (): JSX.Element => {
+  const { t } = useI18n();
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.inner}>
+        <nav>
+          <Link to={Page.HOME} className={styles.link}>
+            <MdOutlineHomeWork className={styles.icon} />
+          </Link>
+          <NavLink
+            to={Page.COUNTRIES}
+            className={({ isActive }) => (isActive ? styles.linkActive : styles.link)}
+          >
+            {t('main.countries')}
+          </NavLink>
+          <NavLink
+            to={Page.CITIES}
+            className={({ isActive }) => (isActive ? styles.linkActive : styles.link)}
+          >
+            {t('main.cities')}
+          </NavLink>
+        </nav>
+        <Language />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Header;
